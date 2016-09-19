2b80136f86f923a678b5b10d0800525366611c77
SeanCox <sean.cox@mac.com>
2016-09-19 12:12:40 -0700

    [feature]created middleware to console log route

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
-
 const app = express()
+
+//loggs route to console
+app.get('/*', (req, res, next)=>{
+  console.log(`Request.Url: ${req.url}`)
+  next()
+})
 
 app.use('/', express.static('public'))
 
-
-const port = 1337
+const port = process.env.PORT || 1337
 
 app.listen(port, ()=>{
   console.log(`app is listing on ${port}`)
