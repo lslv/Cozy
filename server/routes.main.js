ea643ffd24c019d85a7cb8f6a873a6fb8effff59
SeanCox <sean.cox@mac.com>
2016-09-19 16:00:13 -0700

    [setup]user login and signup route framework

--- server/routes.main.js
+++ server/routes.main.js
@@ -1,20 +1,21 @@
 const express = require('express')
-const router = express.Router()
 const chalk = require('chalk')
+const router = express.Router()
 
+//route files
 const billing = require('./billing/routes.billing')
 const bulletinBoard = require('./bulletinBoard/routes.bulletinBoard')
 const chores = require('./chores/routes.chores')
 const users = require('./users/routes.users')
 
-//loggs route to console
+//loggs url to console on request
 router.get('/*', (req, res, next)=>{
   console.log(chalk.blue(`Request Url: ${req.url}`))
   next()
 })
 
+//routes
 router.use('/', express.static('public'))
-
 router.use('/api/billing', billing)
 router.use('/api/bulletinBoard', bulletinBoard)
 router.use('/api/chores', chores)
