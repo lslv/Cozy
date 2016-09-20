200ab1ac387dde8180c5d7b9bc1ddf48592bcfaf
Lee Mordell <lee.mordell@gmail.com>
2016-09-20 10:29:39 -0700

    [pull]added fontawesome to index.html built bulletin board container

--- Public/src/components/BulletinBoard.jsx
+++ Public/src/components/BulletinBoard.jsx
@@ -4,9 +4,20 @@ import BulletinPost from './BulletinPost'
 export default class BulletinBoard extends Component {
   render () {
     return (
-      <p>
-        This is the bulletin board
-      </p>
+      <div className='container'>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>
+                Add a post-it <i className='fa fa-plus-circle' aria-hidden='true'></i>
+              </th>
+              <th>
+                Create a poll <i className='fa fa-plus-circle' aria-hidden='true'></i>
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     )
   }
 }
