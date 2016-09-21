690d84907ad9bb92c9532cb1ce16565a2b494cea
vvnsze <vvnsze@gmail.com>
2016-09-21 09:42:44 -0700

    [pull]removed day of week out of chores and used chalk

--- Server/chores/model.chores.js
+++ Server/chores/model.chores.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const sequelize = require('../config/database')
 let Houses = require('../houses/model.houses.js')
 let Users = require('../users/model.users.js')
+let chalk = require('chalk')
 
 // This is the chore Table itself
 const Chores = sequelize.define('chores', {
@@ -9,37 +10,32 @@ const Chores = sequelize.define('chores', {
     type: Sequelize.STRING(50),
     allowNull: false
   },
-  day_of_week: {
-    type: Sequelize.ARRAY(Sequelize.TEXT),
-    allowNull: false
-  },
-  user_turn: {
+  user_turn:{
     type: Sequelize.INTEGER
   }
 },
-  {
-    timestamps: true,
-    // createdAt: 'created_at',
-    // updatedAt: 'updated_at',
-    // deletedAt: 'deleted_at',
-    underscored: true,
-    paranoid: true
-  })
+{
+  timestamps: true,
+  underscored: true,
+  paranoid: true
+})
 
 Chores.belongsTo(Houses)
 
 Chores.sync().then(function () {
   // Table created
-  console.log('+++line32 model.chores table successfully created')
-}).catch(function (err) {
+  console.log(chalk.yellow('+++line32 model.chores table successfully created'))
+}).catch(function(err){
   console.error('There was an error in model.chores', err)
 })
 
 // This is the Chore Days
 
-const Chore_Days = sequelize.define('chore_days', {
-  // Days will be inputed as "monday, tuesday, wednesday, thursday, friday, saturday, sunday"
-  day: {
+//This is the Chore Days
+
+const Chore_Days = sequelize.define('chore_days',{
+  //Days will be inputed as "monday, tuesday, wednesday, thursday, friday, saturday, sunday"
+  day:{
     type: Sequelize.STRING(10),
     unique: true,
     notNull: true
@@ -50,34 +46,29 @@ Chore_Days.belongsTo(Chores)
 
 Chore_Days.sync().then(function () {
   // Table created
-  console.log('+++line53 model.Chore_Days table successfully created')
-}).catch(function (err) {
+  console.log(chalk.yellow('+++line53 model.Chore_Days table successfully created'))
+}).catch(function(err){
   console.error('There was an error in model.chores.Chore_Days', err)
 })
 
-// This table keeps track of whether a chore is completed by a user
-const Chore_Completions = sequelize.define('chore_completions', {
-  chore_id: {
+//This table keeps track of whether a chore is completed by a user
 
-  },
-  user_id: {
-
-  },
-  verifying_user_id: {
-
-  },
-  complete: {
+const Chore_Completions = sequelize.define('chore_completions',{
+  verified:{
     type: Sequelize.BOOLEAN
-  }
+  },
+  completed:{
+    type: Sequelize.BOOLEAN
+  },
 },
-  {
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: false,
-    deletedAt: false
-  })
+{
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false,
+  deletedAt: false,
+})
 
-Chore_Completions.belongsTo(Chores, {
+Chore_Completions.belongsTo(Chores,{
   as: 'chore_id',
   foreignKey: 'Chores'
 })
@@ -89,16 +80,17 @@ Chore_Completions.belongsTo(Users, {
 
 Chore_Completions.sync().then(function () {
   // Table created
-  console.log('+++line83 model.Chore_Completions table successfully created')
-}).catch(function (err) {
+  console.log(chalk.yellow('+++line83 model.Chore_Completions table successfully created'))
+}).catch(function(err){
   console.error('There was an error in model.chores.Chore_Completions', err)
 })
 
-// This Table is the queues the order of user turns
-const Queues = sequelize.define('queues', {
-  turn: {
+//This Table is the queues the order of user turns
+
+const Queues = sequelize.define('queues',{
+  turn:{
     type: Sequelize.INTEGER
-  // allowNull: false
+    // allowNull: false
   }
 })
 
@@ -114,8 +106,8 @@ Queues.belongsTo(Chores, {
 
 Queues.sync().then(function () {
   // Table created
-  console.log('+++line104 model.chores.queues table successfully created')
-}).catch(function (err) {
+  console.log(chalk.yellow('+++line104 model.chores.queues table successfully created'))
+}).catch(function(err){
   console.error('There was an error in model.chores.queues', err)
 })
 
