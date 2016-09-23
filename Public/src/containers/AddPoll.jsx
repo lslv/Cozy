b15db7ff183c7e1ce0e1174a29bfce520a6d4f26
Lee Mordell <lee.mordell@gmail.com>
2016-09-23 12:05:25 -0700

    [setup]create a poll component renders on bulletin board

---
+++ Public/src/containers/AddPoll.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AddPoll extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <h1>It shows up</h1>
+    )
+  }
+}
