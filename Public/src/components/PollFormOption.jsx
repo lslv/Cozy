2834db77b39a3ab9e67baa3f010cafe49b52128b
Lee Mordell <lee.mordell@gmail.com>
2016-09-23 15:12:48 -0700

    [pull]can dynamically add poll options

---
+++ Public/src/components/PollFormOption.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => {
+  return (
+    <div className='form-group'>
+      <label>
+        Option
+        {props.optionNumber}
+      </label>
+      <input type='text' className='form-control' placeholder={props.optionNumber} />
+    </div>
+  )
+}
--- Public/src/containers/AddPoll.jsx
+++ Public/src/containers/AddPoll.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
+import { Button, Collapse, Well } from 'react-bootstrap'
+import { reduxForm } from 'redux-form'
+
+import PollFormOption from '../components/PollFormOption'
 
 export default class AddPoll extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      pollOptionIterator: [],
+      counter: 1
+    }
+
+    this.addOption = this.addOption.bind(this)
+    this.renderNewOption = this.renderNewOption.bind(this)
+  }
+
+  addOption () {
+    this.setState({ pollOptionIterator: [...this.state.pollOptionIterator, '1']})
+
+    this.renderNewOption()
+  }
+
+  renderNewOption () {
+    return this.state.pollOptionIterator.map((option, i) => {
+      i += 1
+      return (
+        <PollFormOption key={i} optionNumber={i} />
+      )
+    })
   }
 
   render () {
     return (
-      <h1>It shows up</h1>
+      <form>
+        <div className='form-group'>
+          <label>
+            Ask a question:
+          </label>
+          <input type='text' className='form-control' placeholder='Whats your question?' />
+        </div>
+        <div className='form-group'>
+          <Button bsStyle='primary' onClick={this.addOption}>
+            Add option: <i className='fa fa-plus-circle' aria-hidden='true'></i>
+          </Button>
+        </div>
+        {this.renderNewOption()}
+      </form>
     )
   }
 }
