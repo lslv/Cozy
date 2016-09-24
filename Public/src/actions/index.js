ba21ec870682060c933c2f0886009c188f167193
Lee Mordell <lee.mordell@gmail.com>
2016-09-24 10:54:45 -0700

    [pull]progress on implementing redux-forms to poll feature

--- Public/src/actions/index.js
+++ Public/src/actions/index.js
@@ -20,67 +20,67 @@ export function addPost (postData) {
   // Dummy data
   // postData.username = 'Lee'
   // postData.house_id = '2'
-  const request = axios.post('/api/bulletinBoard/addPost', {
-    title: postData.title,
-    message: postData.message
-  })
-  return {
-    type: ADD_POST,
-    payload: request
-  }
+	const request = axios.post('/api/bulletinBoard/addPost', {
+		title: postData.title,
+		message: postData.message
+	})
+	return {
+		type: ADD_POST,
+		payload: request
+	}
 }
 
 export function getChores (house_id = 1) { // harcoded in house ID
-  console.log('getting a chore action')
-  const payload = axios.get('http://localhost:1337/api/chores/getChores', {
-  params: {house_id}}) // hardcoded in localhost
-  return {
-    type: GET_CHORES,
-    payload: payload
-  }
+	console.log('getting a chore action')
+	const payload = axios.get('http://localhost:1337/api/chores/getChores', {
+		params: {house_id}}) // hardcoded in localhost
+	return {
+		type: GET_CHORES,
+		payload: payload
+	}
 }
 
 export function addChore (choreData) {
-  console.log('adding a chore action')
-  choreData.user_turn = 0 // hardcoded user with user 1
-  choreData.house_id = 1 // hardcoded house Id of 1
-  choreData.num_of_users = 4 // hardcoded number of users to be 4
-  const payload = axios.post('http://localhost:1337/api/chores/postChore', choreData) // hardcoded in local host
-  return {
-    type: ADD_CHORE,
-    payload: payload
-  }
+	console.log('adding a chore action')
+	choreData.user_turn = 0 // hardcoded user with user 1
+	choreData.house_id = 1 // hardcoded house Id of 1
+	choreData.num_of_users = 4 // hardcoded number of users to be 4
+	const payload = axios.post('http://localhost:1337/api/chores/postChore', choreData) // hardcoded in local host
+	return {
+		type: ADD_CHORE,
+		payload: payload
+	}
 }
 
 export function deleteChore (choreId) {
-  console.log('deleting a chore action')
-  console.log(choreId)
-  const payload = axios.delete('http://localhost:1337/api/chores/deleteChore', {
-  params: {id: choreId}}) // hardcoded in local host
-  return {
-    type: DELETE_CHORE,
-    payload: payload
-  }
+	console.log('deleting a chore action')
+	console.log(choreId)
+	const payload = axios.delete('http://localhost:1337/api/chores/deleteChore', {
+		params: {id: choreId}}) // hardcoded in local host
+	return {
+		type: DELETE_CHORE,
+		payload: payload
+	}
 }
 
 export function updateChoreTurn (choreId) {
-  console.log('updating chore# ' + choreId)
-  const payload = axios.put('http://localhost:1337/api/chores/updateChoreTurn', {id: choreId})
-  return {
-    type: UPDATE_CHORE,
-    payload: payload
-  }
+	console.log('updating chore# ' + choreId)
+	const payload = axios.put('http://localhost:1337/api/chores/updateChoreTurn', {id: choreId})
+	return {
+		type: UPDATE_CHORE,
+		payload: payload
+	}
 }
 
 export function getQueue (choreId) {
-  console.log('grabbing a chore queue')
+	console.log('grabbing a chore queue')
   // console.log(choreId)
-  const payload = axios.get('http://localhost:1337/api/chores/getQueue', {
-  params: {choreId: choreId}}) // hardcoded in local host
-  return {
-    type: GET_QUEUE,
-    payload: payload
-  }
+	const payload = axios.get('http://localhost:1337/api/chores/getQueue', {
+		params: {choreId: choreId}}) // hardcoded in local host
+	return {
+		type: GET_QUEUE,
+		payload: payload
+	}
 }
 
 export function updateQueue (choreId) {
@@ -94,35 +94,35 @@ export function updatePosts () {
   // for testing purposes, get all where title = title
   // Eventually, get all where house_id matches the user's house_id
 
-  const testQuery = '/api/bulletinBoard/getPosts'
-  const request = axios.get(testQuery, {
-    params: {
-      title: 'title'
-    }
-  })
-  return {
-    type: GET_POSTS,
-    payload: request
-  }
+	const testQuery = '/api/bulletinBoard/getPosts'
+	const request = axios.get(testQuery, {
+		params: {
+			title: 'title'
+		}
+	})
+	return {
+		type: GET_POSTS,
+		payload: request
+	}
 }
 
 export function editPost (post, updatedMessage) {
-  const query = `/api/bulletinBoard/editPost`
-  const request = axios.put(query, {
-    id: post.id,
-    message: updatedMessage
-  })
-  return {
-    type: EDIT_POST,
-    payload: request
-  }
+	const query = '/api/bulletinBoard/editPost'
+	const request = axios.put(query, {
+		id: post.id,
+		message: updatedMessage
+	})
+	return {
+		type: EDIT_POST,
+		payload: request
+	}
 }
 
 export function deletePost (post) {
-  const query = `/api/bulletinBoard/deletePost?id=${post.id}`
-  const request = axios.delete(query)
-  return {
-    type: DELETE_POST,
-    payload: post
-  }
+	const query = `/api/bulletinBoard/deletePost?id=${post.id}`
+	const request = axios.delete(query)
+	return {
+		type: DELETE_POST,
+		payload: post
+	}
 }
--- Public/src/containers/AddPoll.jsx
+++ Public/src/containers/AddPoll.jsx
@@ -5,35 +5,34 @@ import { reduxForm } from 'redux-form'
 import PollFormOption from '../components/PollFormOption'
 
 export default class AddPoll extends Component {
-  constructor (props) {
-    super(props)
+	constructor (props) {
+		super(props)
 
-    this.state = {
-      pollOptionIterator: [],
-      counter: 1
-    }
+		this.state = { counter: 1 }
 
-    this.addOption = this.addOption.bind(this)
-    this.renderNewOption = this.renderNewOption.bind(this)
-  }
+		this.addOption = this.addOption.bind(this)
+		this.renderNewOption = this.renderNewOption.bind(this)
+	}
 
-  addOption () {
-    this.setState({ pollOptionIterator: [...this.state.pollOptionIterator, '1']})
 
-    this.renderNewOption()
-  }
+	addOption () {
+		this.state[`Option ${this.state.counter}`] = ''
+    this.setState({ counter: this.state.counter+=1})
+		console.log('this.state', this.state)
+		// this.renderNewOption()
+	}
 
-  renderNewOption () {
-    return this.state.pollOptionIterator.map((option, i) => {
-      i += 1
-      return (
-        <PollFormOption key={i} optionNumber={i} />
-      )
-    })
-  }
+	renderNewOption () {
+		// return this.state.iterator.map((option, i) => {
+		// 	i+=1
+		// 	return (
+  //       <PollFormOption key={i} optionNumber={i}/>
+  //     )
+		// })
+	}
 
-  render () {
-    return (
+	render () {
+		return (
       <form>
         <div className='form-group'>
           <label>
@@ -46,8 +45,14 @@ export default class AddPoll extends Component {
             Add option: <i className='fa fa-plus-circle' aria-hidden='true'></i>
           </Button>
         </div>
-        {this.renderNewOption()}
+        {/*this.renderNewOption()*/}
+        {Object.keys(this.state).map((field) => {
+          if(field !== 'counter') {
+            <label key={field}>
+              <input type='text' value={this.state[field]} onChange={event => this.setState({ [field]: event.target.value })} /> 
+          }
+        })}
       </form>
     )
-  }
+	}
 }
