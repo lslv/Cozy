e3b727a8c8a1ec2146bb9ab7c860c5c82232cc8c
Lucas Fisher <lsfisher@usc.edu>
2016-09-26 15:24:12 -0700

    [feature] added styling to chore visualization

--- Public/src/containers/Queue.jsx
+++ Public/src/containers/Queue.jsx
@@ -4,76 +4,71 @@ import vis from 'vis'
 export default class Queue extends Component{
 	constructor(props){
 		super(props)
-		console.log('queue props in queue component',this.props.queues)
+		// console.log('queue props in queue component',this.props.queues)
+		this.state={
+			network:null,
+			nodes:null,
+			edges:null
+		}
 	}
 	componentDidMount() {
-            // create an array with nodes
+		console.log('after component did mount')
 		const {queues} = this.props
 		const {chore} = this.props
 		const {users}= this.props 
-		// var nodes = new vis.DataSet([
-  //       {id: 1, label: 'Node 1'},
-  //       {id: 2, label: 'Node 2'},
-  //       {id: 3, label: 'Node 3'},
-  //       {id: 4, label: 'Node 4'},
-  //       {id: 5, label: 'Node 5'}
-		// ])
-
 		var choreQueue= queues[this.props.chore.id]
 		var queueInOrder=[ ...choreQueue.slice(chore.user_turn), ...choreQueue.slice(0, chore.user_turn) ]
 		var nodes= queueInOrder.map((queuePosition,index)=>{        
-			// if(index< queues[this.props.chore.id].length-1){
-
-   //          }
-
+			if(index===0){
+				return {id:users[queuePosition.userId].id, label: users[queuePosition.userId].user_name, color:'lime' }
+			}
 			return {id:users[queuePosition.userId].id, label: users[queuePosition.userId].user_name }
-			// 	return (
-   //                  <span key={queuePosition.id}>
-   //                  { `${users[queuePosition.userId].user_name}\'s` } Turn {'<'}-  
-   //                  </span>
-   //                  )
-			// else
-   //              return (
-   //                  <span key={queuePosition.id}>
-   //                  { `${users[queuePosition.userId].user_name}\'s` } Turn 
-   //                  </span>
-   //                  )
 		})
-
 		nodes= new vis.DataSet(nodes)
+		this.setState({nodes})
 
 		var edges=[]
 
 		for(var i=0;i < queueInOrder.length;i++){
-			if(i < queueInOrder.length-1)
-				edges.push({from:queueInOrder[i].userId, to:queueInOrder[i+1].userId})
+			if(i < queueInOrder.length-1){
+				edges.push({from:queueInOrder[i].userId, to:queueInOrder[i+1].userId, arrows:'from'})
+			}
 			else
-                edges.push({from:queueInOrder[i].userId, to:queueInOrder[0].userId})
+                edges.push({from:queueInOrder[i].userId, to:queueInOrder[0].userId, arrows:'from'})
 		}
-
-    // create an array with edges
-		// var edges = new vis.DataSet([
-  //       {from: 1, to: 3},
-  //       {from: 1, to: 2},
-  //       {from: 2, to: 4},
-  //       {from: 2, to: 5}
-		// ])
+		this.setState({edges})
 		var edges = new vis.DataSet(edges)
 
     // create a network
 		var container = document.getElementById('mynetwork'+this.props.chore.id)
-
     // provide the data in the vis format
 		var data = {
 			nodes: nodes,
 			edges: edges
 		}
-		var options = {}
+		var options = {
+			edges:{
+				color:{
+					inherit:false,
+					color:'#97C2FC',
+					highlight:'#97C2FC'
+				}
+			},
+			nodes:{
+				shape:'dot',
+				size:20
+			}
+		}
 
     // initialize your network!
 		var network = new vis.Network(container, data, options)
+		var nodeIdList=nodes.map((node)=> node.id )
+		network.fit({nodes:nodes.map((node)=> node.id )})
+		
+		this.setState({network})
           
 	}
+
 	render(){
 		return (
             <div className="network" id={`mynetwork${this.props.chore.id}`}>
