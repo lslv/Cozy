e27363a98050a1a1516dc5b62d342380622b8838
Lucas Fisher <lsfisher@usc.edu>
2016-10-08 17:38:09 -0700

    [feature] added spinner to calendar creation

--- Public/src/containers/ChoreList.jsx
+++ Public/src/containers/ChoreList.jsx
@@ -21,6 +21,7 @@ class ChoreList extends Component {
 			SCOPES: ['https://www.googleapis.com/auth/calendar'],
 			makeButtonStyle:{display:'none'},
 			authButtonStyle:{display:'none'},
+			loaderStyle: {display:'none'},
 			upcomingEventsStyle:{display:'none'},
 			upcomingChores:[]
 		}
@@ -93,7 +94,7 @@ class ChoreList extends Component {
 
 	listUpcomingChores() { //list upcoming chores
 		console.log('listUpcomingChores')
-		this.setState({makeButtonStyle:{display:'none'}, upcomingEventsStyle:{display:'inline'}})
+		this.setState({loaderStyle:{display:'none'}, upcomingEventsStyle:{display:'inline'}})
 		//currently have it set to the wrong calendar, also need to filter to see if the  username is included in the chore title
 		var request = gapi.client.calendar.events.list({
 			'calendarId': this.props.calendar,
@@ -146,7 +147,7 @@ class ChoreList extends Component {
 
 	//functions invovled in making a calendar
 	handleMakeCalendarClick(event){
-		this.setState({makeButtonStyle:{display:'none'}} )
+		this.setState({makeButtonStyle:{display:'none'}, loaderStyle:{display:'inline-block'}} )
 		gapi.client.load('calendar', 'v3', this.createNewCalendar)
 	}
 
@@ -277,7 +278,7 @@ class ChoreList extends Component {
 				        <Button className="makeCalButton" style={this.state.makeButtonStyle} id="create-button" onClick={ event=>this.handleMakeCalendarClick(event)}>
 				          Make Cozy Google Calendar
 				        </Button>
-				        
+				        <img className="spinner small" style={this.state.loaderStyle} src='/../../cozy_loading.gif' />
 						<div className="upcomingChores" style={this.state.upcomingEventsStyle}>
 							<h1>Upcoming Chores</h1>
 							<a href="https://calendar.google.com/calendar/iphoneselect" target="_blank">Sync Calendar With Phone</a>
