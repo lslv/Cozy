350a0a5f7fadd3a79d8622af90a305595c383572
SeanCox <sean.cox@mac.com>
2016-10-13 12:53:17 -0700

    [deploy] Fixed typo

--- Public/src/index.js
+++ Public/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './components/App'
+import App from './components/app'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import ReduxPromise from 'redux-promise'
